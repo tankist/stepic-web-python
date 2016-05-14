0d0a8273266b57360c0d6c45edc6caa50b09bd64
Victor Gryshko <vgryshko@brightgrove.com>
2016-05-15 02:35:18 +0300

    Added embed nginx role

--- ansible/server.sh
+++ ansible/server.sh
@@ -3,11 +3,8 @@
 # Update Repositories
 
 # Add Ansible Repository & Install Ansible
-sudo add-apt-repository -y ppa:ansible/ansible
+#sudo add-apt-repository -y ppa:ansible/ansible
 sudo apt-get update
 sudo apt-get install -y software-properties-common ansible git
 
-#Galaxy roles installation
-sudo ansible-galaxy install -r ./install_roles.yml
-
-sudo ansible-playbook ./playbook.yml -e hostname=stepic.lan --connection=local -v
+sudo ansible-playbook ./server_playbook.yml -e hostname=stepic.lan --connection=local -v
