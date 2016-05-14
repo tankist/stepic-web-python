8237df57fe9d485a7dfa67a00d2493a68879ae4a
Victor Gryshko <vgryshko@brightgrove.com>
2016-05-15 02:39:01 +0300

    Added ansible hosts

--- ansible/server.sh
+++ ansible/server.sh
@@ -7,4 +7,9 @@
 sudo apt-get update
 sudo apt-get install -y software-properties-common ansible git
 
+# Setup Ansible for Local Use and Run
+sudo cp ./inventories/dev /etc/ansible/hosts -f
+sudo chmod 666 /etc/ansible/hosts
+cat ./files/authorized_keys >> /home/box/.ssh/authorized_keys
+
 sudo ansible-playbook ./server_playbook.yml -e hostname=stepic.lan --connection=local -v
