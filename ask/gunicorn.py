340483fb8cae4f1f2b46387e50c5a6bce25e35a1
Victor Gryshko <vgryshko@brightgrove.com>
2016-05-22 01:27:37 +0300

    Initial Django setup

---
+++ ask/gunicorn.py
@@ -0,0 +1,5 @@
+bind = "0.0.0.0:8000"
+workers = 10
+pythonpath = "/home/box/web/ask"
+errorlog = "/home/box/web/etc/ask-error.log"
+loglevel = "debug"
---
+++ install.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+cd ansible
+bash server.sh
+
+sudo service stepic start
