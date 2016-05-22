9bec9224220de1a89ffb0ab8286e16e89e1d7ffa
Victor Gryshko <vgryshko@brightgrove.com>
2016-05-23 02:26:03 +0300

    Added QuestionsManager and some field options

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -5,16 +5,23 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 
+class QuestionManager (models.Manager):
+    def new(self):
+        return self.order_by('-added_at')
+    def popular(self):
+        return self.order_by('-rating');
+
 class Question (models.Model):
+    objects = QuestionManager()
     title = models.CharField(max_length=255)
     text = models.TextField()
-    added_at = models.DateField()
-    rating = models.IntegerField()
+    added_at = models.DateField(blank=True)
+    rating = models.IntegerField(default=0)
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='authors')
     likes = models.ManyToManyField(User)
 
 class Answer (models.Model):
     text = models.TextField()
-    added_at = models.DateField()
+    added_at = models.DateField(blank=True)
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
