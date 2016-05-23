6abba447153bbe453e8ad82831e2bb8489186845
Victor Gryshko <vgryshko@brightgrove.com>
2016-05-24 01:40:01 +0300

    Added default now

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -1,6 +1,7 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from datetime import datetime
 from django.contrib.auth.models import User
 
 # Create your models here.
@@ -15,13 +16,13 @@ class Question (models.Model):
     objects = QuestionManager()
     title = models.CharField(max_length=255)
     text = models.TextField()
-    added_at = models.DateField(blank=True)
+    added_at = models.DateField(default=datetime.now)
     rating = models.IntegerField(default=0)
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='authors')
     likes = models.ManyToManyField(User)
 
 class Answer (models.Model):
     text = models.TextField()
-    added_at = models.DateField(blank=True)
+    added_at = models.DateField(default=datetime.now)
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
